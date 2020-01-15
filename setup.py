3ceb1660e894ce45332ddfb914339ecc5d4a8a4a
ali <alialimohamed475@gmail.com>
2020-01-15 15:22:57 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='GCPConfig',
+    version="1.0.1",
+    author='Ali Mohamed',
+    packages=["GCPConfig"],
+    author_email='alialimohamed475@gmail.com',
+    install_requires=[
+        "google-cloud-storage"
+    ]
+)
